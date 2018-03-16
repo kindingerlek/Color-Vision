14ddc434d5123c75bce6a041277e9987c37476ac
Lucas Ernesto Kindinger <kindingerlek@hotmail.com>
2018-03-16 20:35:33 -0300

    add some comments to receiver script

--- Assets/Scripts/ParticlesReceiver.cs
+++ Assets/Scripts/ParticlesReceiver.cs
@@ -3,42 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticlesReceiver : MonoBehaviour {
+    // Enable print in cosole some useful information
     [SerializeField] private bool debug;
 
+    // Flashlighter
     [SerializeField] private ParticleSystem   redEmmitter;
     [SerializeField] private ParticleSystem greenEmmitter;
     [SerializeField] private ParticleSystem  blueEmmitter;
     
+    
     [SerializeField, Range(0.1f, 2f)]
-    private float colorChangePerception = 1.5f;
+    // This is the speed to change the current color to a new color generated
+    private float colorChangePerception = 1.5f; 
 
-    [SerializeField, Range(1, 30)]
+    [SerializeField]
+    // Set the 'Max Photons' per channel to use when generate a new color
     private int channelMax = 15;
 
+    // Store the photons collision
     private List<ParticleCollisionEvent>   redPhotonCollisions = new List<ParticleCollisionEvent>();
     private List<ParticleCollisionEvent> greenPhotonCollisions = new List<ParticleCollisionEvent>();
     private List<ParticleCollisionEvent>  bluePhotonCollisions = new List<ParticleCollisionEvent>();
 
-
+    // Current color
     private Color color;
-
-    // Use this for initialization
-    void Start () {
-
-	}
-	
+    
 	// Update is called once per frame
 	void Update () {
+        // Read the collision of photons to this receiver
         ParticlePhysicsExtensions.GetCollisionEvents(   redEmmitter, gameObject,   redPhotonCollisions);
         ParticlePhysicsExtensions.GetCollisionEvents( greenEmmitter, gameObject, greenPhotonCollisions);
         ParticlePhysicsExtensions.GetCollisionEvents(  blueEmmitter, gameObject,  bluePhotonCollisions);
         
+        // Smooth change the color to new color
         color = Color.Lerp(color, GetColor(), colorChangePerception * Time.deltaTime);
 
+        // Enable the debug
         if (debug)
             DebugThis();
     }
 
+    // Print in Console some useful informations
     void DebugThis()
     {
         Debug.Log(string.Format(
@@ -49,10 +54,12 @@ public class ParticlesReceiver : MonoBehaviour {
              ColorUtility.ToHtmlStringRGB(color)));
     }
 
+    // Generate a new color based in how much photons were absorbed by this receiver
     private Color GetColor()
     {
         Color c = new Color();
 
+        // Clamp the values between the limits [0;channelMax] and them normalize it (value between 0 and 1)
         c.r = Mathf.Clamp(   redPhotonCollisions.Count, 0, channelMax) / (float) channelMax;
         c.g = Mathf.Clamp( greenPhotonCollisions.Count, 0, channelMax) / (float) channelMax;
         c.b = Mathf.Clamp(  bluePhotonCollisions.Count, 0, channelMax) / (float) channelMax;
