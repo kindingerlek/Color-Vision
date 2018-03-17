2a97424470a6600419e54038f43c71f078c17209
Lucas Ernesto Kindinger <kindingerlek@hotmail.com>
2018-03-16 22:31:00 -0300

    add a balloon to show the color

---
+++ Assets/Scripts/UIControllers/MultipleFlashlightsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleFlashlightsController : MonoBehaviour {
+    [SerializeField]
+    SpriteRenderer thinkingBalloonSprite;
+
+    [SerializeField]
+    ParticlesReceiver receiver;
+
+    void Update()
+    {
+        thinkingBalloonSprite.color = receiver.GetColor();
+    }
+}
