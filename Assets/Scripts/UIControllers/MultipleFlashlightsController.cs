8465981c6d7902d5541596551d46a82c2e41b0fd
Lucas Ernesto Kindinger <kindingerlek@hotmail.com>
2018-03-16 23:14:45 -0300

    add timeControl slider

--- Assets/Scripts/UIControllers/MultipleFlashlightsController.cs
+++ Assets/Scripts/UIControllers/MultipleFlashlightsController.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MultipleFlashlightsController : MonoBehaviour {
     [SerializeField]
-    SpriteRenderer thinkingBalloonSprite;
+    SpriteRenderer colorBallon;
 
     [SerializeField]
     ParticlesReceiver receiver;
 
+    [SerializeField]
+    Slider timeSpeed;
+
+    private void Start()
+    {
+        if(!colorBallon)
+            colorBallon = GameObject.Find("Color Balloon").GetComponent<SpriteRenderer>();
+
+        if (!receiver)
+            receiver = GameObject.FindObjectOfType<ParticlesReceiver>();
+
+        if (!timeSpeed)
+            timeSpeed = this.transform.Find("TimeControl/Slider").GetComponent<Slider>();
+
+        
+    }
+
     void Update()
     {
-        thinkingBalloonSprite.color = receiver.GetColor();
+        colorBallon.color = receiver.GetColor();
+        Time.timeScale = timeSpeed.value;
     }
 }
