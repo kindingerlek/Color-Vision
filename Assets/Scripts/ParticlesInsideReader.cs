d95019dd36e900a342861e65e8f824f14080cbd6
kindingerlek <kindingerlek@hotmail.com>
2018-03-18 20:46:01 -0300

    create script to get inside particle trigger event and process it

---
+++ Assets/Scripts/ParticlesInsideReader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class ParticlesInsideReader : MonoBehaviour
+{
+    private new ParticleSystem particleSystem;
+
+    public IPhotonProcessor photonProcessor;
+
+    // particles
+    List<ParticleSystem.Particle> buffer = new List<ParticleSystem.Particle>();
+
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        
+        // Set-Up the particle system to callback when particle Inside in filter
+        var trigger = particleSystem.trigger;
+        trigger.enabled = true;
+        trigger.inside = ParticleSystemOverlapAction.Callback;
+    }
+
+    void OnParticleTrigger()
+    {
+        if (photonProcessor == null)
+            return;
+        
+        particleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Inside, buffer);
+        photonProcessor.Process(buffer);        
+        particleSystem.SetTriggerParticles(ParticleSystemTriggerEventType.Inside, buffer);
+        
+    }
+}
